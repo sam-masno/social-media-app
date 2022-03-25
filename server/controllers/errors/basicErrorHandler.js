d2e7af470e7178d7abca9f0565259bef5a2f1aa3
sam <sammasno@gmail.com>
2022-03-25 21:11:12 +0400

    added log

--- server/controllers/errors/basicErrorHandler.js
+++ server/controllers/errors/basicErrorHandler.js
@@ -1,4 +1,4 @@
 module.exports = (err, req, res, next) => {
-    console.log('error handler hit')
+    console.log(err.message);
     res.status(err.status || 500).json({success: false, message: err.message || 'Server Error'});
 }
