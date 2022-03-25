81634f26f602ee9b5f063a21cec3e78d1c17d781
sam <sammasno@gmail.com>
2022-03-25 19:39:35 +0400

    implemented createPost function with author

--- server/controllers/posts/createPost.js
+++ server/controllers/posts/createPost.js
@@ -1,17 +1,16 @@
-const { asyncHandler, uploads: uploadsPath } = require('../../utils');
+const { asyncHandler, imagePaths: {uploads} } = require('../../utils');
 const { Post } = require('../../db/models');
 const { writeFile, unlink } = require('fs').promises;
 const { v4: uuid } = require('uuid');
 
-module.exports = asyncHandler((req, res, next) => {
-    return res.json(req.user);
+module.exports = asyncHandler(async (req, res, next) => {
     const { title, description } = req.body;
 
     const errors = [];
 
-    if(!title) errors.push('Valid title required');
+    if(!title || title.length < 2 || title.length > 50) errors.push('Title must be between 2 and 50 characters.');
 
-    if(!description) errors.push('Description is required.')
+    if(!description || description.length < 5 || description.length > 500) errors.push('Description must be between 5 and 500 characters.')
 
     if(!req.files[0] || req.files[0].mimetype.split('/')[0] !== 'image') {
         errors.push('Image is invalid.');
@@ -23,6 +22,17 @@ module.exports = asyncHandler((req, res, next) => {
 
     const { buffer } = req.files[0];
 
+    const uploadName = uuid();
+    const uploadPath = `${uploads}/${uploadName}`
+    const imageUrl = '/images/uploads/' + uploadName;
 
-    res.json(req.body);    
+    await writeFile(uploadPath, buffer);
+
+    try {
+        const post = await Post.create({ author: req.user, title, description, imageUrl });
+        res.json({ success: true, post });    
+    } catch (error) {
+        await unlink(uploadPath);
+        throw error;
+    }
 });
