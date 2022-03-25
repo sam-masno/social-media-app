af048e72ff60433d649ed1dc28e7b12954888fce
sam <sammasno@gmail.com>
2022-03-25 21:43:46 +0400

    added is post owner middleware

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     handleJwt: require('./handleJwt'),
-    protected: require('./protected')
+    protected: require('./protected'),
+    isPostOwner: require('./isPostOwner')
 };
---
+++ server/middleware/isPostOwner.js
@@ -0,0 +1,18 @@
+const { Post } = require('../db/models');
+const { asyncHandler } = require('../utils');
+
+module.exports = asyncHandler(async (req, res, next) => {
+    const { postId } = req.params;
+
+    const post = await Post.findById(postId);
+
+    if(!post) throw new Error('Post does not exist');
+
+    if(!post.author.toString() === req.user._id.toString()) {
+        throw new Error('Only the author of this post can perform this action');
+    }
+
+    req.post = post;
+
+    next();
+});
