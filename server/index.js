2ffae69ae238a47e1404cad783bb49bdd0529407
sam <sammasno@gmail.com>
2022-03-25 21:10:57 +0400

    added file serve from public

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 require('./db/connectDB');
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const upload = require('multer')();
@@ -10,6 +11,7 @@ app.use(upload.any());
 app.use(bodyParser.json());
 
 app.use('/api', require('./routes'));
+app.use('/images', express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/errors/basicErrorHandler'));
 
 app.listen(process.env.PORT || 3001, () => console.log('App running'));
