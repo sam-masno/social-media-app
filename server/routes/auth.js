8fec8ac67bda96485f4512297eb324750f48e4e0
sam <sammasno@gmail.com>
2022-03-25 18:31:50 +0400

    removed multer

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 const { registerNewUser, login } = require('../controllers');
-const upload = require('multer')();
 
 router.route('/register')
-    .post(upload.any(), registerNewUser);
+    .post(registerNewUser);
 
 router.route('/login')
     .post(login);
