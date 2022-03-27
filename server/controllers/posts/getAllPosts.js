64f7116eeb6767e9bcf8be9dcdc7aff535a817b5
sam <sammasno@gmail.com>
2022-03-27 15:03:07 +0400

    added basic tag search

--- server/controllers/posts/getAllPosts.js
+++ server/controllers/posts/getAllPosts.js
@@ -2,6 +2,10 @@ const { Post } = require('../../db/models');
 const { asyncHandler } = require('../../utils');
 
 module.exports = asyncHandler(async(req, res, next) => {
-    const posts = await Post.find().populate({path: 'author', select:'-password'});
+    const { tag } = req.query;
+
+    const query = tag ? { tags: tag.toLowerCase() } : {};
+    
+    const posts = await Post.find(query).populate({path: 'author', select:'-password'});
     res.json({ success: true, posts });
 });
